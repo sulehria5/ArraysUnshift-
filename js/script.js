66e9b06a5d54f2c459af6b70d9b1805238b2cf37
ali raza sulehria <alirazasulehria@gmail.com>
2019-10-31 14:27:39 +0100

    ArraysUnshift()

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var deutscheGerichte = ["Speckkuchen"," Thüringer Rostbratwurst", "Quarkkeulchen", "Sauerbraten"];
+
+    deutscheGerichte.unshift("Matjesfilet mit Speckstippe");
+    
+          
+  console.log(deutscheGerichte);
